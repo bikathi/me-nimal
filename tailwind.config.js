1e1072a8232276aebee2fe70552c6d7a510cb71e
Bikathi Martin <bikathimartin@outlook.com>
2023-09-01 12:11:02 +0300

    Extend font and scale properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["index.html", "./src/**/*.{html, js}"],
+	content: ["index.html", "./src/**/*.{html, js}", "./assets/*.js"],
 	theme: {
 		extend: {
-      fontFamily: {
-        josefin: ['Josefin Sans', "sans-serif"]
-      }
-    },
+			fontFamily: {
+				josefin: ["Josefin Sans", "sans-serif"],
+				inter: ["Inter", "sans-serif"],
+				poppins: ["Poppins", "sans-serif"],
+			},
+			scale: {
+				102: "1.02",
+			},
+		},
 	},
 	plugins: [],
 };
